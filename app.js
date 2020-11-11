c98fa6918edb2bb970c4a4f74ad1ee89c7cdd932
abhishek-shah-vst-au4 <54413214+abhishek-shah-vst-au4@users.noreply.github.com>
2020-11-11 22:02:45 +0530

    Fixed error

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use("/user", require("./routes/user"));
 
-// SetUping the Error Handling
-const errorHandlers = require("./handlers/errorHandler");
+//Setup Error Handlers
+const errorHandlers = require("./handlers/errorHandlers");
 app.use(errorHandlers.notFound);
 app.use(errorHandlers.mongoseErrors);
 if (process.env.ENV === "DEVELOPMENT") {
-  app.use(errorHandlers.productionErrors);
+  app.use(errorHandlers.developmentErrors);
 } else {
   app.use(errorHandlers.productionErrors);
 }
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,10 @@
 const { route } = require("../app");
 
 const router = require("express").Router();
-const {catchErrors} = require("../handlers/errorHandler");
-const userController = require("../controllers/userController")
+const { catchErrors } = require("../handlers/errorHandlers");
+const userController = require("../controllers/userController");
 
-router.post("/login", catchErrors(userController.login))
-router.post("/register", catchErrors(userController.register))
-
+router.post("/login", catchErrors(userController.login));
+router.post("/register", catchErrors(userController.register));
 
 module.exports = router;
