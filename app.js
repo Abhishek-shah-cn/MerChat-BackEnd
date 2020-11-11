787d5882b7c43c05d1cd349af1181757cd8d921b
abhishek-shah-vst-au4 <54413214+abhishek-shah-vst-au4@users.noreply.github.com>
2020-11-11 18:55:20 +0530

    Adding the mongoDB

--- app.js
+++ app.js
@@ -5,4 +5,19 @@ const app = express();
 app.use(express.json)
 app.use(express.urlencoded({extended:true}));
 
+
+// SetUping the Error Handling
+const errorHandlers = require("./handlers/errorHandler");
+app.use(errorHandlers.notFound);
+app.use(errorHandlers.mongoseErrors);
+if(process.env.ENV === "DEVELOPMENT")
+{
+    app.use(errorHandlers.productionErrors);
+} else {
+    app.use(errorHandlers.productionErrors);
+}
+
+
+
+
 module.exports = app;
--- server.js
+++ server.js
@@ -1,7 +1,21 @@
-const app = require('./app');
+require("dotenv").config();
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("Mongoose Connection ERROR:" + err.message);
+});
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB Connected");
+});
+
+const app = require("./app");
 const port = 8000;
 
-app.listen(port , function(err){
-    console.log("server is runing")
-})
-
+app.listen(port, function (err) {
+  console.log("server is runing on port:-" + port);
+});
